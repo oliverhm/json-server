2804de6f5a45df667bb1679f7adf85936f6ca6c7
Oliver Henrique Maceira <oliver@ciandt.com>
2021-04-29 19:51:53 -0300

    Update code.

--- api/index.js
+++ api/index.js
@@ -1,14 +1,15 @@
 const app = require("express")();
 const bodyParser = require("body-parser");
 const fs = require("fs");
+const { join } = require("path");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-const dataPath = "./server.json";
+const dataPath = "../episodes.json";
 
 app.get("/api/episodes", (req, res) => {
-  fs.readFile(dataPath, "utf8", (err, data) => {
+  fs.readFile(join(__dirname, dataPath), "utf8", (err, data) => {
     if (err) {
       throw err;
     }
